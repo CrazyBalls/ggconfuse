fbb0b33023f42738ed3d0cdc9481dc00671856d1
CrazyBalls <326676300@qq.com>
2018-07-06 17:41:48 +0800

    增加动态生成垃圾代码

--- gxlconfusion.py
+++ gxlconfusion.py
@@ -8,18 +8,20 @@ sys.setdefaultencoding('utf8')
 import os
 import random
 
-TheProjectPath = ''  #项目路径 直接脱进来
-ThePrefix_New = 'NEW'   #类名新前缀
-ThePrefix_Old = 'OLD'   #类名旧前缀
+TheProjectPath = '/Users/guoxiaolei/Desktop/wealth/LDAccount-master/LZAccount/'  #项目路径 直接脱进来
+ThePrefix_New = 'SFSM'   #类名新前缀
+ThePrefix_Old = 'MM'   #类名旧前缀
 TheJunkCode_type = '' #
 TheJunkCode_count = 5  #生成垃圾代码方法数量
 TheJunkCode_methodslength =  16 #生成垃圾代码方法长度
 TheJunkCode_outlength =  10    #生成垃圾代码方法输出长度
-TheJunkCode_Dir  = ['Assets.xcassets','Base.lproj','xcodeproj','第三方','Pods','icons']  #过滤输入目录名即可
+TheJunkCode_Dir  = ['Assets.xcassets','Base.lproj','xcodeproj','第三方','Pods','SFSMAccount.xcworkspace','SFSMAccountTests','SFSMAccountUITests','icons']  #过滤输入目录名即可
 
 TheJunkCode_Filesuffix = ['.DS_Store','main.m','json','Info.plist']  #过滤文件名
 
-
+TheJunkFilePath = '/Users/guoxiaolei/Desktop/wealth/LED-master/LED/LED/Model/'  #垃圾文件目录
+PCH_Path = '/Users/guoxiaolei/Desktop/wealth/LED-master/LED/LED/PrefixHeader.pch' #PCH路径
+TheJunkFilePathCount = 5  #文件数量（.h.m为1个）
 
 
 alljunkfile = []  #垃圾代码文件目录
@@ -27,6 +29,8 @@ alljunkfile = []  #垃圾代码文件目录
 classMFiles = [] #全局.m文件拿到
 
 classHMFiles = []  #全局.hm文件拿到
+
+junkClassFiles = [] #全局垃圾文件记录放到PCH导入
 #类名前缀修改
 def modifyClassName():
     for tmpMFile in classMFiles:
@@ -123,13 +127,13 @@ def getClassNameArr(Path):
 def addJunkCode():
     for tmpFile in alljunkfile:
         junkcodeStr = generate_random_str(tmpFile)
-        # file = open(tmpFile,"r+")
-        # fileContent = file.readlines()
-        # for line in fileContent:
-        #     line_new = line.replace('@end', junkcodeStr + '\n@end')
-        #     print  line_new
-        #     file.write(line_new)
-        # file.close()
+        file = open(tmpFile,"r+")
+        fileContent = file.readlines()
+        for line in fileContent:
+            line_new = line.replace('@end', junkcodeStr + '\n@end')
+            print  line_new
+            file.write(line_new)
+        file.close()
 
 def productCode(Path):
     allfilelist = os.listdir(Path)
@@ -189,18 +193,68 @@ def generate_random_str(filetype):
     except Exception, e:
         print ("JunkCode: %s-------%s" % (e,filetype))
 
+#生成文件
+def product_junkFile():
+    base_str2 = 'abcdefghigklmnopqrstuvwxyz'
+    try:
+        for index in range(TheJunkFilePathCount):
+            fileName = ThePrefix_New
+            for i in range(8):
+                fileName += base_str2[random.randint(0, len(base_str2) - 1)]
+            file = open(TheJunkFilePath + fileName + '.h', 'w')
+            fileContent = '#import <Foundation/Foundation.h>\n '
+            fileContent += '@interface %s : NSObject \n' % (fileName)
+            for ii in range(random.randint(1, 9)):
+                fileContent += product_junkVar()
+            fileContent += '@end'
+            file.write(fileContent)
+
+            file = open(TheJunkFilePath + fileName + '.m', 'w')
+            fileContent = '#import "%s"\n ' % (fileName+'.h')
+            fileContent += '@implementation %s \n' % (fileName)
+            fileContent += '@end'
+            file.write(fileContent)
+            file.close()
+            junkClassFiles.append('#import "%s"'% (fileName+'.h'))
+    except Exception, e:
+        print ("JunkCode: %s-------%s" % (e))
+
+#生成随机变量
+def product_junkVar():
+    base_str2 = 'abcdefghigklmnopqrstuvwxyz'
+    var = ''
+    for i in range(10):
+        var += base_str2[random.randint(0, len(base_str2) - 1)]
+    var = '@property (nonatomic, copy)NSString     *%s; \n'%(var)
+    return var
+
+#把垃圾文件插入PCH中
+def insertjunkFiletoPch():
+    print  junkClassFiles
+    importStr = ''
+    for tmpFile in junkClassFiles:
+        importStr+=tmpFile+'\n'
+    file = open(PCH_Path, "r+")
+    fileContent = file.readlines()
+    for line in fileContent:
+        line_new = line.replace('#endif ', importStr + '\n#endif ')
+        file.write(line_new)
+    file.close()
+
 #启动开始
 try:
-    print ('获取代码文件')
-    productCode(TheProjectPath)
-    print ('开始添加混淆代码')
-    addJunkCode()
-    print ('混淆代码添加完毕')
-    print ('获取所有M类名')
-    #getClassNameArr(TheProjectPath)
-    print ('开始修改类名前缀')
-    #modifyClassName()
-    print ('类名前缀修改完毕')
+    # print ('获取代码文件')
+    # productCode(TheProjectPath)
+    # print ('开始添加混淆代码')
+    # addJunkCode()
+    # print ('混淆代码添加完毕')
+    # print ('获取所有M类名')
+    # #getClassNameArr(TheProjectPath)
+    # print ('开始修改类名前缀')
+    # #modifyClassName()
+    # print ('类名前缀修改完毕')
+    product_junkFile() #生成垃圾文件
+    insertjunkFiletoPch() #pch写入
 except Exception, e:
     print ("Error: %s"%(e))
 
